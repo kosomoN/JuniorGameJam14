332140289ea0f3e25b9c17b102e01d1739fe9b2a
Onni <onni.kosomaa@hotmail.com>
2014-10-18 15:18:00 +0300

    Added ghostsssssss

--- core/src/com/juniorjam/crawler/game/GameState.java
+++ core/src/com/juniorjam/crawler/game/GameState.java
@@ -6,13 +6,21 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Array;
 
 public class GameState extends ScreenAdapter {
 	
+	private static final float TICK_LENGTH = 1000000000 / 60f;
+	
+	private float unprocessed;
+	private long lastTickTime;
+	private int ticks;
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
 	private DungeonMap map;
 	private Player player;
+	private Array<Player> ghosts = new Array<Player>();
+	
 	
 	public GameState(SpriteBatch batch, OrthographicCamera camera) {
 		this.batch = batch;
@@ -24,23 +32,71 @@ public class GameState extends ScreenAdapter {
 		map = new DungeonMap("maps/Test.tmx", batch);
 		
 		
-		player = new Player(0, 0);
+		player = new Player(64, 64, 0, this);
 	}
 
 	@Override
 	public void render(float delta) {
 		super.render(delta);
 		
-		player.update(delta, map);
+		long currTime = System.nanoTime();
+        unprocessed += (currTime - lastTickTime) / TICK_LENGTH;
+        lastTickTime = currTime;
+        while(unprocessed >= 1) {
+        	unprocessed--;
+        	update();
+        	ticks++;
+        }
 		
+		render();
+		
+		
+	}
+
+	private void update() {
+		
+		for(Player p : ghosts)
+			p.update(map);
+		
+		if(player.update(map)) {
+			player.kill();
+			
+			ghosts.add(player);
+			player = new Player(64, 64, ticks, this);
+			
+			for(Player p : ghosts)
+				p.respawnAsGhost();
+		}
+	}
+
+	private void render() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		camera.position.set(player.x, player.y, 0);
 		
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		map.render(camera);
 		
 		batch.begin();
+		
+		for(Player p : ghosts)
+			p.render(batch);
+		
 		player.render(batch);
 		batch.end();
+	}
+	
+	@Override
+	public void show() {
+		super.show();
+		
+		unprocessed = 0;
+		lastTickTime = System.nanoTime();
+	}
+	
+
+	public int getCurrentTick() {
+		return ticks;
 	}
 }
